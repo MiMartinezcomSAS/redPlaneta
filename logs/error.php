a193c5ad3d664b5816bd0f8f06c93bd31bab058a
juan2ramos <juan2ramos@gmail.com>
2014-05-20 12:05:13 -0500

    + commit initial

---
+++ logs/error.php
@@ -0,0 +1,59 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-01-21 03:12:56 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-01-21T03:12:56+00:00	INFO	joomlacanceled	
+2014-01-21T03:13:21+00:00	INFO	joomlacanceled	
+2014-01-21T03:14:10+00:00	INFO	joomlacanceled	
+2014-01-21T03:19:44+00:00	INFO	joomlacanceled	
+2014-01-21T03:22:20+00:00	INFO	joomlacanceled	
+2014-01-21T03:22:45+00:00	INFO	joomlacanceled	
+2014-01-21T03:36:48+00:00	INFO	joomlacanceled	
+2014-01-21T03:37:02+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-21T03:37:14+00:00	INFO	joomlacanceled	
+2014-01-21T03:45:24+00:00	INFO	joomlacanceled	
+2014-01-21T17:35:39+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:00:36+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:08:27+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:08:40+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:08:48+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:08:52+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:09:00+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:09:28+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T03:31:47+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T16:32:55+00:00	INFO	joomlafailure	No se permiten contraseñas vacías
+2014-01-22T16:33:34+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T23:13:32+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-22T23:13:59+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-23T20:22:48+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-23T20:25:45+00:00	INFO	joomlacanceled	
+2014-01-23T20:26:53+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-24T02:09:08+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-24T22:17:04+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-01-24T22:17:20+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-02-08T17:13:24+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-02-13T15:59:45+00:00	INFO	joomlafailure	No se permiten contraseñas vacías
+2014-02-13T15:59:53+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-02-22T00:42:18+00:00	INFO	joomlafailure	No se permiten contraseñas vacías
+2014-02-22T00:42:35+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-02-22T00:42:58+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-02-25T13:50:49+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-02-25T13:51:19+00:00	INFO	joomlacanceled	
+2014-02-25T13:51:48+00:00	INFO	joomlacanceled	
+2014-03-03T23:34:51+00:00	INFO	joomlafailure	No se permiten contraseñas vacías
+2014-03-03T23:41:13+00:00	INFO	joomlacanceled	
+2014-03-05T20:08:21+00:00	INFO	joomlafailure	No se permiten contraseñas vacías
+2014-03-18T16:49:06+00:00	INFO	joomlafailure	No se permiten contraseñas vacías
+2014-03-19T14:26:44+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-03-21T16:46:23+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-03-25T23:39:05+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-03-25T23:39:39+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-03-26T00:48:26+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-03-26T01:28:47+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-03-26T03:41:27+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-03-31T23:16:17+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-04-06T16:36:14+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-04-06T16:36:26+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2014-04-07T15:13:08+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
